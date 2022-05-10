e5ccf146e3d01c3eee211ce7ca812ec7bd27ff50
Fermin <fermingarciabargas@icloud.com>
2022-05-10 10:10:25 -0300

    Agregar archivos de proyecto.

---
+++ Principios SOLID/Ej1/Vehiculo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios_SOLID
+{
+    class Vehiculo
+    {
+        protected int cantRuedas;
+        protected int velocidadMax;
+        protected int coloresDisponibles;
+        public Vehiculo()
+        {
+        }
+        public Vehiculo(int cantRuedas, int velocidadMax, int coloresDisponibles)
+        {
+            this.CantRuedas = cantRuedas;
+            this.VelocidadMax = velocidadMax;
+            this.ColoresDisponibles = coloresDisponibles;
+        }
+        public int CantRuedas { get => cantRuedas; set => cantRuedas = value; }
+        public int VelocidadMax { get => velocidadMax; set => velocidadMax = value; }
+        public int ColoresDisponibles { get => coloresDisponibles; set => coloresDisponibles = value; }
+
+        //se mezcla la estructura de la clase, con la presentacion o muestra de la clase, seria mejor crear 
+        //una clase exclusiva para la muestra, o una interfaz dependiendo del caso.
+        public void mostrarVehiculo()          
+        {
+            Console.WriteLine("Ruedas: " + this.cantRuedas + ", Velocidad Maxima: " + this.velocidadMax + ", Colores disponibles: " + this.coloresDisponibles);
+        }
+
+    }
+
+    abstract class MuestraVehiculo : Vehiculo
+    {
+        public void mostrarVehiculo()
+        {
+            Console.WriteLine("Ruedas: " + cantRuedas + ", Velocidad Maxima: " + velocidadMax + ", Colores disponibles: " + coloresDisponibles);
+        }
+    }
+}
---
+++ Principios SOLID/Ej2/Rectangulo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principios_SOLID
+{
+    interface FigurasGeometricas
+    {
+        double CalculoArea();
+    }
+    class Rectangulo : FigurasGeometricas
+    {
+        private double baseRec;
+        private double altura;
+
+        public Rectangulo()
+        {
+        }
+
+        public Rectangulo(double baseRec, double altura)
+        {
+            this.baseRec = baseRec;
+            this.altura = altura;
+        }
+
+        public double BaseRec { get => baseRec; set => baseRec = value; }
+        public double Altura { get => altura; set => altura = value; }
+
+        public double CalculoArea()
+        {
+            return this.Altura * this.BaseRec;
+        }
+    }
+
+    class CalcularArea
+    {
+        //la clase CalcularArea solo es util para rectangulos,pero si necesito calcular el area de cualquier otra
+        //figura geometria no tiene utilidad, por eso seria mejor implementar una interfaz con el metodo calcular area
+        //y en caso de necesitarlo, darle a cada figura su propio metodo de calcularArea.
+        public double calcularArea(Rectangulo rec)
+        {
+            return rec.Altura * rec.BaseRec;
+        }
+    }
+
+    class Circulo : FigurasGeometricas
+    {
+        private double radio;
+        public double CalculoArea()
+        {
+            return (Math.PI * (this.radio * this.radio));
+        }
+    }
+}
